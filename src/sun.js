21813f370880a897b3ebf9ab7853d27c09e6ef76
liwl <liwl@Ctrip.com>
2016-01-15 19:30:10 +0800

    update

--- src/sun.js
+++ src/sun.js
@@ -697,7 +697,7 @@ sun.location = {
 
         var _hostname = host + ':' + port;
         var _origin = protocol + '//' + host + ':' + port;
-        var _search = '?' + url;
+        var _search = !!url ? '?' + url : '';
         var _pathname = '/' + path;
         var _href = _origin + _pathname + _search;
 
@@ -801,14 +801,14 @@ sun.location = {
             });
 
             // 如果_queryString为空的话，history.replaceState不可执行
-            if (_queryString.indexOf('?') < 0) {
+            if (!_.isEmpty(_queryString) && _queryString.indexOf('?') < 0) {
                 _queryString += '?' + _queryString
             }
 
-            resultUrl = _queryString;
+            resultUrl = _url.origin + _url.pathname + _queryString;
         }
 
-        return _url.origin + _url.pathname + resultUrl;
+        return resultUrl;
     }
 };
 
